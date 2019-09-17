cb299d0e3b4e7be876d6287f6fa186af8682fd86
RobbieVibart <robvibart@gmail.com>
2019-09-17 15:06:31 +0200

    All_REV_Ver17

--- js/script.js
+++ js/script.js
@@ -1,7 +1,68 @@
-// --img-OBSERVER -- START --//
+// // --img-OBSERVER -- START -- OLD//
+// let options = {
+//     root: null,
+//     rootMargin: "-600px 0px 600px 0px",
+//     threshold: 0, 
+// };
+
+// let observer = new IntersectionObserver(intersect, options);
+
+// document.querySelectorAll('div, .right').forEach(img => {
+// observer.observe(img);
+// // console.log('watching', img);
+
+// });
+
+// function intersect(entries) {
+//     entries.forEach(entry => {
+//         if(entry.isIntersecting) {
+//         // console.log('is intersecting', entry);
+//         // console.log(entry.time, entry.intersectionRatio);
+//         entry.target.classList.add('appearRight');
+//     } else {
+//         // console.log('not intersecting', entry);
+//         entry.target.classList.remove('appearRight');
+//         // observer.unobserve(img);
+//     }
+// }); 
+// }
+// // --img OBSERVER -- END -- OLD//
+
+// // --COPY OBSERVER -- START -- OLD//
+
+let fadeOptions = {
+    root: null,
+    rootMargin: "0px 0px",
+    threshold: 0, 
+};
+
+let imageObserver = new IntersectionObserver(fadeIntersect, fadeOptions);
+
+document.querySelectorAll('.fade_up').forEach(p => {
+    imageObserver.observe(p);
+    // console.log('watching', p);
+    
+});
+
+function fadeIntersect(entries) {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            // console.log('intersecting');
+            // console.log(entry.time, entry.intersectionRatio);
+            entry.target.classList.add('active');
+        } else {
+            entry.target.classList.remove('active');
+            // imageObserver.unobserve(p);
+        }
+    });
+}
+// // --COPY OBSERVER -- END --  OLD//
+
+
+// --COPY-OBSERVER -- START --//
 let options = {
     root: null,
-    rootMargin: "-600px 0px 600px 0px",
+    rootMargin: "0px 0px",
     threshold: 0, 
 };
 
@@ -9,51 +70,56 @@ let observer = new IntersectionObserver(intersect, options);
 
 document.querySelectorAll('div, .right').forEach(img => {
 observer.observe(img);
-// console.log('watching', img);
+// console.log('watching');
 
 });
 
 function intersect(entries) {
     entries.forEach(entry => {
         if(entry.isIntersecting) {
-        // console.log('is intersecting', entry);
+        // console.log('intersecting');
         // console.log(entry.time, entry.intersectionRatio);
         entry.target.classList.add('appearRight');
-    } else {
-        // console.log('not intersecting', entry);
-        entry.target.classList.remove('appearRight');
-        // observer.unobserve(img);
-    }
-}); 
-}
-// --img OBSERVER -- END --//
-
-// --COPY OBSERVER -- START --//
-
-let textOptions = {
-    root: null,
-    rootMargin: "0px 0px 0px 0px",
-    threshold: 1, 
-};
-
-let imageObserver = new IntersectionObserver(textIntersect, textOptions);
-
-document.querySelectorAll(' div, .left').forEach(p => {
-    imageObserver.observe(p);
-    // console.log('watching', p);
-    
-});
-
-function textIntersect(entries) {
-    entries.forEach(entry => {
-        if(entry.isIntersecting) {
-            // console.log('intersecting');
-            // console.log(entry.time, entry.intersectionRatio);
-            entry.target.classList.add('appearLeft');
         } else {
-            entry.target.classList.remove('appearLeft');
-            // imageObserver.unobserve(p);
-        }
-    });
+                !entry.target.classList.remove('appearRight');
+					// console.log('not intersecting');
+                // observer.unobserve(imgs);
+					
+										
+
+			}
+	}); 
 }
+
 // --COPY OBSERVER -- END --//
+
+// --IMAGE OBSERVER -- START --//
+
+// let textOptions = {
+//     root: null,
+//     rootMargin: "0px 0px 0px 0px",
+//     threshold: 0, 
+// };
+
+// let imageObserver = new IntersectionObserver(textIntersect, textOptions);
+
+// document.querySelectorAll('div, .left').forEach(p => {
+// imageObserver.observe(p);
+// // console.log('watching', p);
+
+// });
+
+// function textIntersect(entries) {
+//     entries.forEach(entry => {
+//         if(entry.isIntersecting) {
+//         // console.log('intersecting');
+//         // console.log(entry.time, entry.intersectionRatio);
+//         entry.target.classList.add('appearLeft');
+//         } else {
+// 				entry.target.classList.remove('appearLeft');
+//                 // imageObserver.unobserve(p);
+// 			}
+//     });
+// }
+// --IMAGE OBSERVER -- END --//
+
